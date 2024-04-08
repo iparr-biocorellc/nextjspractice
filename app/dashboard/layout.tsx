5e6c4bf1bbdbb41a14d06eb100679085f747d8ee
iparr-biocorellc <106994145+iparr-biocorellc@users.noreply.github.com>
2024-04-08 08:33:09 -0400

    style tweak

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -6,7 +6,7 @@ export default function Layout({ children }: { children: React.ReactNode }) {
             <div className="w-full flex-none md:w-64">
                 <SideNav />
             </div>
-            <div className="flex-grow p-6 overflow-hidden md:p-12">{children}</div>
+            <div className="flex-grow p-6 overflow-scroll md:p-12">{children}</div>
         </div>
     );
 }
